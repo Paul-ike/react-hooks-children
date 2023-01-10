a7e18a16027d907ffb5c80c6b26a379a976824eb
Paul <paul.ngugi@student.moringaschool.com>
2023-01-10 23:14:18 +0300

    Completed assignment

--- src/components/Container.js
+++ src/components/Container.js
@@ -6,7 +6,7 @@ function Container({
   textPosition = "", // possible values: left, right, center
   direction = "horizontal", // possible values: vertical, horizontal
   contentPosition = "center", // possible values: left, right, center
-}) {
+}) { 
   return (
     <div className={`container ${textPosition}`}>
       {header ? <h2>{header}</h2> : null}
